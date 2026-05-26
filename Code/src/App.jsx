8feabba656ad5e4b6a1a0221a39a9d158c414f32
Solar-Gamer-pulse <gaminganuragfromnameram@gmail.com>
2026-05-26 21:47:11 +0530

    Clean up Base44 branding, add Google Ads, switch to user site

--- Code/src/App.jsx
+++ Code/src/App.jsx
@@ -45,7 +45,7 @@ export default function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router basename="/Solar-Gamer-Pulse">
+        <Router>
           <AuthenticatedApp />
         </Router>
         <Toaster />
