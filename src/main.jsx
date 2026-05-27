b9504b2d3c2643cc319d82ca7e2995762e257335
Solar-Gamer-pulse <gaminganuragfromnameram@gmail.com>
2026-05-27 15:57:47 +0530

    Add horizontal ad unit (728x90) to index.html

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,4 @@ import ReactDOM from "react-dom/client";
 import App from "@/App.jsx";
 import "@/index.css";
 
-const script = document.createElement("script");
-script.src =
-  "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2679829741412830";
-script.crossOrigin = "anonymous";
-script.async = true;
-document.head.appendChild(script);
-
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
